fe81d6580598f0d0f245627487d6f9a58803aa81
DreamMoon <1067823908@qq.com>
2023-09-16 15:52:50 +0800

    READMEUPDATA

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # BlueSky
 
-Developed with Unreal Engine 4
+Developed with Unreal Engine 4.27
+
+# A1Project
+DreamMoon
+带有 "A1 Project" 标识的项目,均为学习项目
+
+此项目用于
+UC++学习
+UImGui 学习
--- Source/BlueSky/Private/ImGuiActor.cpp
+++ Source/BlueSky/Private/ImGuiActor.cpp
@@ -28,7 +28,7 @@ void AImGuiActor::BeginPlay()
 #if WITH_IMGUI
 	//OnWorldDebug
 	//委托给ImGui调试事件。在世界更新期间，世界代表每帧被调用一次，在他们的世界失效后，调用列表被清除。每个更新的世界都会调用多上下文委托一次。早期调试委托在世界tick开始时被调用，调试委托在世界post actor tick或4.18以下的引擎版本中在世界tick开始时被调用。事件的顺序以一种多上下文委托可以用来绘制页眉或页脚的方式定义:多上下文早期调试，世界早期调试，世界调试，多上下文调试。
-	FImGuiDelegates::OnWorldDebug().AddUObject(this, &AImGuiActor::ImGui_Tick);
+	FImGuiDelegates::OnWorldEarlyDebug().AddUObject(this, &AImGuiActor::ImGui_Tick);
 #endif
 	
 	
